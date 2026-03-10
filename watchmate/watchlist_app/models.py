19aa989ff2785982d70cad02d386bc014206e4b8
Programming-Seungwan <swsj4480@gmail.com>
2026-03-10 23:00:34 +0900

    feat : Movie 들의 리스트를 뽑는 Function Based View 작성 후 라우팅 연결

--- watchmate/watchlist_app/models.py
+++ watchmate/watchlist_app/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Movie(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=200)
-    active = models.CharField(default=True)
+    active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
