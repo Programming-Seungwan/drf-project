84f1ca1296bf82eab9638b485104b5b2caf83e26
Programming-Seungwan <swsj4480@gmail.com>
2026-03-15 15:36:41 +0900

    feat : 영화 생성 post 기능 구현

--- watchmate/watchlist_app/api/views.py
+++ watchmate/watchlist_app/api/views.py
@@ -5,12 +5,21 @@ from watchlist_app.api.serializers import MovieSerializer
 
 # Create your views here.
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def movie_list(request):
-    movies = Movie.objects.all()
-    serializer = MovieSerializer(movies, many=True)
+    if request.method == 'GET':
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
 
-    return Response(serializer.data)
+        return Response(serializer.data)
+
+    if request.method == 'POST':
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save() # 내부적으로 Serializer 인스턴스의 create() 메서드를 호출한다.
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
 
 @api_view(['GET'])
 def movie_detail(request, pk):
