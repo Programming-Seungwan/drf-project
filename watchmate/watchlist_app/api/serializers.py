21e9596e83efdfea06025f2d51d4401ecfc3d6e1
Programming-Seungwan <swsj4480@gmail.com>
2026-03-15 15:54:48 +0900

    feat : serializer 기능을 이용해서 put, delete 기능 구현

--- watchmate/watchlist_app/api/serializers.py
+++ watchmate/watchlist_app/api/serializers.py
@@ -10,3 +10,11 @@ class MovieSerializer(serializers.Serializer):
 
     def create(self, validated_data):
         return Movie.objects.create(**validated_data)
+
+    def update(self, instance, validated_data): # instance는 기존 DB 저장 데이터를 의미, validated_data는 요청으로부터의 데이터
+        instance.name = validated_data.get('name', instance.name)
+        instance.description = validated_data.get('description', instance.description)
+        instance.active = validated_data.get('active', instance.active)
+        instance.save()
+
+        return instance
--- watchmate/watchlist_app/api/views.py
+++ watchmate/watchlist_app/api/views.py
@@ -21,9 +21,25 @@ def movie_list(request):
         else:
             return Response(serializer.errors)
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def movie_detail(request, pk):
-    movies = Movie.objects.get(pk= pk)
-    serializer = MovieSerializer(movies)
+    if request.method == 'GET':
+        movies = Movie.objects.get(pk= pk)
+        serializer = MovieSerializer(movies)
 
-    return Response(serializer.data)
+        return Response(serializer.data)
+
+    if request.method == 'PUT':
+        movie = Movie.objects.get(pk=pk)
+        serializer = MovieSerializer(movie, request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+    if request.method == 'DELETE':
+        movie = Movie.objects.get(pk=pk)
+        movie.delete()
+
+        return Response()
