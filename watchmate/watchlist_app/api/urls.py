fdbe982f4ac339b09cb9f8e5a4024b2915db3df9
Programming-Seungwan <swsj4480@gmail.com>
2026-03-26 23:45:36 +0900

    refactor : FBV를 CBV로 전환

--- watchmate/watchlist_app/api/urls.py
+++ watchmate/watchlist_app/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from watchlist_app.api.views import movie_list, movie_detail
+# from watchlist_app.api.views import movie_list, movie_detail
+from watchlist_app.api.views import MovieListView, MovieDetailView
 
 
 urlpatterns = [
-    path('list/', movie_list, name='movie-list'),
-    path('<int:pk>', movie_detail, name='movie-detail')
+    path('list/', MovieListView.as_view(), name='movie-list'),
+    path('<int:pk>', MovieDetailView.as_view(), name='movie-detail')
 ]
--- watchmate/watchlist_app/api/views.py
+++ watchmate/watchlist_app/api/views.py
@@ -3,18 +3,16 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
 from watchlist_app.api.serializers import MovieSerializer
+from rest_framework.views import APIView
 
-# Create your views here.
+class MovieListView(APIView):
 
-@api_view(['GET', 'POST'])
-def movie_list(request):
-    if request.method == 'GET':
+    def get(self, request):
         movies = Movie.objects.all()
         serializer = MovieSerializer(movies, many=True)
-
         return Response(serializer.data)
 
-    if request.method == 'POST':
+    def post(self, request):
         serializer = MovieSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save() # 내부적으로 Serializer 인스턴스의 create() 메서드를 호출한다.
@@ -22,9 +20,9 @@ def movie_list(request):
         else:
             return Response(serializer.errors)
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def movie_detail(request, pk):
-    if request.method == 'GET':
+
+class MovieDetailView(APIView):
+    def get(self, request, pk):
         try:
             movies = Movie.objects.get(pk= pk)
         except Movie.DoesNotExist:
@@ -33,7 +31,7 @@ def movie_detail(request, pk):
         serializer = MovieSerializer(movies)
         return Response(serializer.data)
 
-    if request.method == 'PUT':
+    def put(self, request, pk):
         movie = Movie.objects.get(pk=pk)
         serializer = MovieSerializer(movie, request.data)
         if serializer.is_valid():
@@ -42,8 +40,52 @@ def movie_detail(request, pk):
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    if request.method == 'DELETE':
+    def delete(self, request, pk):
         movie = Movie.objects.get(pk=pk)
         movie.delete()
 
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+# Create your views here.
+
+# @api_view(['GET', 'POST'])
+# def movie_list(request):
+#     if request.method == 'GET':
+#         movies = Movie.objects.all()
+#         serializer = MovieSerializer(movies, many=True)
+
+#         return Response(serializer.data)
+
+#     if request.method == 'POST':
+#         serializer = MovieSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save() # 내부적으로 Serializer 인스턴스의 create() 메서드를 호출한다.
+#             return Response(serializer.data)
+#         else:
+#             return Response(serializer.errors)
+
+# @api_view(['GET', 'PUT', 'DELETE'])
+# def movie_detail(request, pk):
+#     if request.method == 'GET':
+#         try:
+#             movies = Movie.objects.get(pk= pk)
+#         except Movie.DoesNotExist:
+#             return Response({'Error' : 'Movie Not found'}, status=status.HTTP_404_NOT_FOUND)
+
+#         serializer = MovieSerializer(movies)
+#         return Response(serializer.data)
+
+#     if request.method == 'PUT':
+#         movie = Movie.objects.get(pk=pk)
+#         serializer = MovieSerializer(movie, request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         else:
+#             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#     if request.method == 'DELETE':
+#         movie = Movie.objects.get(pk=pk)
+#         movie.delete()
+
+#         return Response(status=status.HTTP_204_NO_CONTENT)
