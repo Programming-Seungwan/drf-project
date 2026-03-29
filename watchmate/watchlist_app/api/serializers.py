8f6da18098fd62c09ad1a249de3618f6f7665cea
Programming-Seungwan <swsj4480@gmail.com>
2026-03-29 11:50:11 +0900

    feat : 기존의 normal serializer를 ModelSerializer로 교체
    1. SerializerMethodField라는 신기한 기능도 drf에서 제공함

--- watchmate/watchlist_app/api/serializers.py
+++ watchmate/watchlist_app/api/serializers.py
@@ -2,33 +2,57 @@ from rest_framework import serializers
 
 from watchlist_app.models import Movie
 
-def name_length(value):
-    if len(value) < 2:
-        raise serializers.ValidationError('name is too short seungwan')
+class MovieSerializer(serializers.ModelSerializer):
+    len_name = serializers.SerializerMethodField()
+    class Meta:
+        model = Movie
+        fields = "__all__"
 
-class MovieSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(validators=[name_length])
-    description = serializers.CharField()
-    active = serializers.BooleanField()
-
-    def create(self, validated_data):
-        return Movie.objects.create(**validated_data)
-
-    def update(self, instance, validated_data): # instance는 기존 DB 저장 데이터를 의미, validated_data는 요청으로부터의 데이터
-        instance.name = validated_data.get('name', instance.name)
-        instance.description = validated_data.get('description', instance.description)
-        instance.active = validated_data.get('active', instance.active)
-        instance.save()
-
-        return instance
+    def get_len_name(self, objects):
+        length = len(objects.name)
+        return length
 
     def validate_name(self, value):
         if len(value) < 2:
             raise serializers.ValidationError("Name is too short")
         else:
             return value
-        
+
     def validate(self, data):
         if data['name'] == data['description']:
             raise serializers.ValidationError('name and Description should not be the same')
+        else:
+            return data
+
+# def name_length(value):
+#     if len(value) < 2:
+#         raise serializers.ValidationError('name is too short seungwan')
+
+# class MovieSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     name = serializers.CharField(validators=[name_length])
+#     description = serializers.CharField()
+#     active = serializers.BooleanField()
+
+#     def create(self, validated_data):
+#         return Movie.objects.create(**validated_data)
+
+#     def update(self, instance, validated_data): # instance는 기존 DB 저장 데이터를 의미, validated_data는 요청으로부터의 데이터
+#         instance.name = validated_data.get('name', instance.name)
+#         instance.description = validated_data.get('description', instance.description)
+#         instance.active = validated_data.get('active', instance.active)
+#         instance.save()
+
+#         return instance
+
+#     def validate_name(self, value):
+#         if len(value) < 2:
+#             raise serializers.ValidationError("Name is too short")
+#         else:
+#             return value
+
+#     def validate(self, data):
+#         if data['name'] == data['description']:
+#             raise serializers.ValidationError('name and Description should not be the same')
+#         else:
+#             return data
