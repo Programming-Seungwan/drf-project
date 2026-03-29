8b299bfee9bce2d74f0d354295be311d029cb664
Programming-Seungwan <swsj4480@gmail.com>
2026-03-29 22:35:04 +0900

    chore : 관계를 맺고 있는 serializer들 간에 nested serializer 구현
    1. StringRelatedField, PrimaryKeyRelatedField 등을 활용해 필요한 필드만을 보내줄 수도 있음

--- watchmate/watchlist_app/api/serializers.py
+++ watchmate/watchlist_app/api/serializers.py
@@ -1,14 +1,15 @@
 from rest_framework import serializers
 from watchlist_app.models import WatchList, StreamingPlatform
 
-class StreamingPlatformSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = StreamingPlatform
-        fields = "__all__"
-
 class WatchListSerializer(serializers.ModelSerializer):
     class Meta:
         model = WatchList
+        fields = "__all__"
+
+class StreamingPlatformSerializer(serializers.ModelSerializer):
+    watchlist = WatchListSerializer(many=True, read_only=True)
+    class Meta:
+        model = StreamingPlatform
         fields = "__all__"
 
 
