71fcf5c6655b870202fdd5c78e807c59fde00f32
Programming-Seungwan <swsj4480@gmail.com>
2026-04-01 00:08:38 +0900

    fix : Review 관련 model 클래스 생성 및 admin에 적용

--- watchmate/watchlist_app/models.py
+++ watchmate/watchlist_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class StreamingPlatform(models.Model):
     name = models.CharField(max_length=30)
@@ -18,3 +19,14 @@ class WatchList(models.Model):
 
     def __str__(self):
         return self.title
+
+class Review(models.Model):
+    rating = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    description = models.CharField(max_length=200, null=True)
+    created = models.DateTimeField(auto_now_add=True) # 객체가 생성될때
+    update = models.DateTimeField(auto_now=True) # 객체가 저장될 때마다 매번
+    active = models.BooleanField(default=True)
+    watchList = models.ForeignKey(WatchList, on_delete=models.CASCADE, related_name='reviews')
+
+    def __str__(self):
+        return str(self.rating) + " | " + self.watchList.title
