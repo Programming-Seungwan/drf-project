07281420cec5e19e75c1892ec70ffd2bea9f57bd
Programming-Seungwan <swsj4480@gmail.com>
2026-05-10 23:03:33 +0900

    feat : drf 기반 token 인증 방식을 구현

--- watchmate/watchmate/urls.py
+++ watchmate/watchmate/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from watchlist_app.api import urls as watchlist_urls
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('watch/', include(watchlist_urls))
+    path('watch/', include(watchlist_urls)),
+    path('api-token-auth/', obtain_auth_token, name='api-token-auth'),
 ]
